25d64f958890c4fdb0112659d92a8b582c01940c
Kel Bucey <kabucey@Kel-Macbook-Pro.local>
2015-07-20 13:17:35 -0400

    Added the ability to plunder through an api.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Installation
+
 Checkout the project and run this command from the project directory:
 
     docker-compose up
+
+# Docker API
+
+Plunder the booty:
+
+    /api/plunder
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json
 from redis import Redis
 
 app = Flask(__name__)
@@ -6,10 +6,23 @@ redis = Redis(host='redis', port=6379, decode_responses=True)
 
 
 @app.route('/')
-def hello():
+def home():
+    plunder()
+    return 'Yarrrr matey!  I have been plundered %s times.  %s booty remains.' % (redis.get('plunders'), redis.get('booty'))
+
+@app.route('/api/plunder')
+def api_plunder():
+    """Plunders the booty and returns a json status."""
+    plunder()
+    dict = {
+        "plunders": int(redis.get('plunders')),
+        "booty": int(redis.get('booty'))
+    }
+    return json.jsonify(dict)
+
+def plunder():
     redis.incr('plunders')
     redis.decr('booty')
-    return 'Yarrrr matey!  I have been plundered %s times.  %s booty remains.' % (redis.get('plunders'), redis.get('booty'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
